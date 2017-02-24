62748e279394cf2917e64d1f6864ffff7b5ee2aa
小小看护 <d13260389574@sina.com>
2017-02-24 15:50:47 +0800

    0224 1550

---
+++ Pingxifu/Pingxifu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pingxifu
+//
+//  Created by newo－du on 17/2/24.
+//  Copyright © 2017年 newo－du. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pingxifu/Pingxifu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pingxifu
+//
+//  Created by newo－du on 17/2/24.
+//  Copyright © 2017年 newo－du. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
